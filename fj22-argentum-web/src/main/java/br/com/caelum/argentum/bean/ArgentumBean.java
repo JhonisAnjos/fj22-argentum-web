accdff5836194a6428078e20e0deb9ff6fdae830
jhonis.anjos <jhonis.anjos@NGTIC005441.agerba.intranet>
2019-01-04 17:07:11 -0300

    v1.0

---
+++ fj22-argentum-web/src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
@@ -0,0 +1,27 @@
+package br.com.caelum.argentum.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.caelum.argentum.modelo.Negociacao;
+import br.com.caelum.argentum.ws.ClienteWebService;
+
+@ManagedBean
+@ViewScoped
+//Sempre que um ManagedBean possuir o escopo maior que o escopo de requisição, ele deverá implementar a interface Serializable:
+public class ArgentumBean implements Serializable {
+
+	private List<Negociacao> negociacoes;
+
+	public ArgentumBean() {
+		negociacoes = new ClienteWebService().getNegociacoes();
+	}
+
+	public List<Negociacao> getNegociacoes() {
+		return negociacoes;
+	}
+
+}
